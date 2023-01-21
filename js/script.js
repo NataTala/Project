43d9f73f634f57773dac014b7c56d8743883e252
nataliia minchenko <i_nat@ukr.net>
2023-01-21 02:09:30 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// "use strict";
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+const name = 'Andrew';
+
+const person = {
+  name: 'Julia',
+  sayHi: function() {
+    return `Hello ${this.name}!`;
+  }
+};
+
+console.log(person.sayHi());
+
